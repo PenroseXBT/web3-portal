b44cf2a105fbb3afb84c2f2e41741eacd9a01cff
o_o <algorithmkillers@aol.com>
2022-04-11 16:54:46 -0400

    GIF Portal

---
+++ app/README.md
@@ -0,0 +1,6 @@
+Solana GIF Portal Project
+
+### **Welcome 👋**
+
+1. Run `npm install` at the root of your directory
+2. Run `npm run start` to start the project
---
+++ app/build/precache-manifest.31e4a3d70a7e071b0d4f0e8b3b44276c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "363c260026af5ae42766",
+    "url": "/static/css/main.0fc02617.chunk.css"
+  },
+  {
+    "revision": "363c260026af5ae42766",
+    "url": "/static/js/main.363c2600.chunk.js"
+  },
+  {
+    "revision": "459db1432bf711913d6e",
+    "url": "/static/js/1.459db143.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c97ee2a3ed91e9de148f8d40b2d324e",
+    "url": "/static/media/twitter-logo.5c97ee2a.svg"
+  },
+  {
+    "revision": "5f0927116d345a02b457d1d4c9d64eff",
+    "url": "/index.html"
+  }
+];
